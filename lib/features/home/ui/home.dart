a7f06ea3dfed01f475d03e67a1f4d8a215cc373d
mridul <mridulvij1569@gmail.com>
2023-06-30 23:12:14 +0530

    code added

--- lib/features/home/ui/home.dart
+++ lib/features/home/ui/home.dart
@@ -2,7 +2,6 @@ import 'package:bloc_architecture/features/home/bloc/home_bloc.dart';
 import 'package:bloc_architecture/features/wishlist/ui/wishlist.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import '../../cart/ui/cart.dart';
 
 class Home extends StatefulWidget {
